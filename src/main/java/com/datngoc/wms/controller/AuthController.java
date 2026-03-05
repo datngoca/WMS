31a439d0f5834a4a72ec911e79b5085d6de8db71
datngoca <datngoc252k3@gmail.com>
2026-03-05 14:12:13 +0700

    feat: add tag for swagger

--- src/main/java/com/datngoc/wms/controller/AuthController.java
+++ src/main/java/com/datngoc/wms/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.datngoc.wms.dto.LoginRequestDTO;
 import com.datngoc.wms.dto.LoginResponseDTO;
 import com.datngoc.wms.service.AuthService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Tag(name ="Authentication", description = "Các api liên quan đến authentication")
 public class AuthController {
 
     private final AuthService authService;
